dd9911a460a9ee1469915863fc91e074a2916284
Sahak-S <sahak.sa@mail.ru>
2022-11-21 23:39:08 +0400

    vaadin crud

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Student {
+
+    @Id
+    @EqualsAndHashCode.Include
+    private Integer regNumber;
+    private String firstName;
+    private String lastName;
+}
